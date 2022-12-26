bee0781e6bea8168f9388b0fb6cfedea7762dfb4
saad ahmed <saadahmed1742@gmail.com>
2022-12-26 21:17:35 +0500

    completed config router

---
+++ src/config/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Admin from '../pages/admin'
+import Signup from '../pages/signup'
+import Login from '../pages/login'
+const Index = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="signup" element={<Signup />}></Route>
+        <Route path="admin" element={<Admin />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Index
